1d45270b9c0d7f8a9e5f145d450f088a15c82e58
daniyial <syeddaniyalali078@gmail.com>
2024-12-04 13:56:43 -0600

    first puch

---
+++ accets/script.js
@@ -0,0 +1,13 @@
+document.querySelector('.navbar-toggler').addEventListener('click', () => {
+    document.querySelector('.mask').style.display = 'block'
+})
+
+document.querySelector('.nav-close').addEventListener('click', () => {
+    document.querySelector('.navbar-collapse').classList.remove('show')
+    document.querySelector('.mask').style.display = 'none'
+})
+
+document.querySelector('.mask').addEventListener('click', () => {
+    document.querySelector('.navbar-collapse').classList.remove('show')
+    document.querySelector('.mask').style.display = 'none'
+})
