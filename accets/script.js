76fc551d7fcaa37661170de685a586adaeab9697
daniyial <syeddaniyalali078@gmail.com>
2025-01-10 12:54:44 -0600

    add

--- accets/script.js
+++ accets/script.js
@@ -10,7 +10,25 @@ document.querySelector('.nav-close').addEventListener('click', () => {
 document.querySelector('.mask').addEventListener('click', () => {
     document.querySelector('.navbar-collapse').classList.remove('show')
     document.querySelector('.mask').style.display = 'none'
+    document.querySelector('.profile-sidenav').style.left = "-100%";
+    document.querySelector('.mask').style.display = "none";
 })
+
+document.querySelectorAll('.profile-sidetog').forEach(element => {
+    element.addEventListener('click', () => {
+        document.querySelector('.profile-sidenav').style.left = "0";
+        document.querySelector('.mask').style.display = "block";
+    })
+});
+
+
+
+document.querySelectorAll('.profile-sideClose').forEach(element => {
+    element.addEventListener('click', () => {
+        document.querySelector('.profile-sidenav').style.left = "-100%";
+        document.querySelector('.mask').style.display = "none";
+    })
+});
 
 
 
